0a0efcb86e67e4ea9edbaaa2ff050ab91dc268ee
Luvixs <148796042+Luvixs@users.noreply.github.com>
2026-02-03 15:42:22 +0100

    test

---
+++ frontend/src/redux/slices/userSlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+
+
+const userSlice = createSlice({
+  
+  initialState: {},
+  reducers: {},
+  extraReducers: () => {}
+
+});
+
+// Esporta le azioni
+//export const { } = userSlice.actions;
+
+// Esporta il reducer
+export default userSlice.reducer;
