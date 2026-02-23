2deb22dbe45d281019656de36df3b27649bda221
Luvixs <148796042+Luvixs@users.noreply.github.com>
2026-02-23 17:08:18 +0100

    store auth

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,9 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './slices/authSlice';
 
-
+/**
+ * Store Redux centrale dell'applicazione.
+ * Contiene tutto lo stato globale diviso in slice.
+ */
 export const store = configureStore({
   reducer: {
-    
+    auth: authReducer,
   }
 });
 
